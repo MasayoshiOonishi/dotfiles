d4efe1e001655371a0a3801e7cfceb5bab0a72d8
MasayoshiOonishi <onishi.masayoshi@itfllc.co.jp>
2022-11-11 10:23:35 +0900

    initialcommit

---
+++ .bash_profile
@@ -0,0 +1,6 @@
+# .bash_profile
+
+#git
+    
+git config --global user.name MasayoshiOonishi
+git config --global user.email onishi.masayoshi@itfllc.co.jp
---
+++ dotfilesLink.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bash_rc ~/.bash_rc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
